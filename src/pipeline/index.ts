2ecfabb0d6e79ee3d3e19a025b6b412bca07431c
yindaheng98 <yindaheng98@gmail.com>
2026-06-15 01:36:40 -0700

    simplify

--- src/pipeline/index.ts
+++ src/pipeline/index.ts
@@ -1,5 +1,4 @@
 export { defineMemoryPipelines } from "./pipeline.js";
-export { defineMemoryRecallPipeline, type MemoryRecallCallback } from "./recall.js";
+export { defineMemoryRecallPipeline, memoryRecallArgsOptions, type MemoryRecallCallback } from "./recall.js";
 export { defineMemoryRememberPipeline, memoryRememberArgsOptions } from "./remember.js";
 export { Memory } from "./memory.js";
-export { memoryRecallArgsOptions } from "./recall.js";
