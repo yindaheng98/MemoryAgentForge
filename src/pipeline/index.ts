e8f7d3b20d2748b0b536faa78eb6ebf9768522a1
yindaheng98 <yindaheng98@gmail.com>
2026-06-15 01:26:30 -0700

    export

--- src/pipeline/index.ts
+++ src/pipeline/index.ts
@@ -1,5 +1,5 @@
 export { defineMemoryPipelines as defineMemoryPipeline } from "./pipeline.js";
 export { defineMemoryRecallPipeline, type MemoryRecallCallback } from "./recall.js";
 export { defineMemoryRememberPipeline, memoryRememberArgsOptions } from "./remember.js";
-export { Memory, type MemoryConfig } from "../memory/index.js";
+export { Memory } from "./memory.js";
 export { memoryRecallArgsOptions } from "./recall.js";
